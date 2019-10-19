e927c1416d12298d7be5fcd1ad4c91b1186dc0c8
snake0 <1260865816@qq.com>
2019-10-19 16:16:35 +0800

    finish cpu_usage

---
+++ cpu-monitor.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+# only works in zsh
+
+# time between reading /proc/stat
+INTERVAL=1
+# log file path
+FILE="cpu_usage.log"
+
+# number of cpu cores
+CPU_NUM=`cat /proc/stat | grep 'cpu[0-9]' -c` 
+
+while true; do
+    # array of cpus
+    startTotalSum=($(cat /proc/stat | grep "cpu[0-9]" | awk '{print $2+$3+$4+$5+$6+$7+$8}' | tr "\n" " "))
+    startIdleSum=($(cat /proc/stat | grep "cpu[0-9]" | awk '{print $5}' | tr "\n" " "))
+
+    sleep $INTERVAL
+
+    # array of cpus
+    stopTotalSum=($(cat /proc/stat | grep "cpu[0-9]" | awk '{print $2+$3+$4+$5+$6+$7+$8}' | tr "\n" " "))
+    stopIdleSum=($(cat /proc/stat | grep "cpu[0-9]" | awk '{print $5}' | tr "\n" " "))
+
+    for i in `seq 1 $CPU_NUM`; do
+        total=$((stopTotalSum[i]-startTotalSum[i]))
+        idle=$((stopIdleSum[i]-startIdleSum[i]))
+        occupied=$((total-idle))
+        occupied_normal=$((occupied*100))
+        percentage[$i]=$((occupied_normal/total))
+    done
+
+    time=$(date +%s)
+    printf "[$time] " >> $FILE
+    for p in $percentage; do
+        printf "%2d " $p >> $FILE
+    done
+    echo >> $FILE
+done
